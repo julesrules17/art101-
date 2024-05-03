7675000d19e32987e5c744de4d89841b6d8f43de
julesrules17 <166079237+julesrules17@users.noreply.github.com>
2024-05-02 19:25:44 -0700

    oh hello

---
+++ lab 7/lab.js
@@ -0,0 +1,31 @@
+/*
+Author: Julia Gompertz <jgompert@ucsc.edu>
+Created: 2 May 2024
+License: Public Domain 
+*/
+
+//sortUserName - a function that takes user input and sorts the letters
+// of their name 
+
+function sortUserName() {
+    var userName = window.prompt("hi! please tell me your name so i can fix it.");
+    console.log("userName =" , userName);
+
+    // split string to array 
+    var nameArray = userName.split('');
+    console.log("nameArray =" , nameArray);
+
+    // sort the array
+    var sameArraySort = nameArray.sort();
+    console.log("nameArraySort =", nameArraySort);
+
+    // join array back to a string 
+    var nameSorted = nameArraySort.join(''); 
+    console.log("nameSorted =", nameSorted); 
+
+    return nameSorted; 
+}
+
+// output
+document.writeln("oh hey, i've fixed your name: ",
+    sortUserName (), "</br>"); 
