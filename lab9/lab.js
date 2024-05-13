d0a23c6234e5376502cab048d76abf83eba39dce
julesrules17 <166079237+julesrules17@users.noreply.github.com>
2024-05-12 21:57:08 -0700

    Update lab.js

--- lab9/lab.js
+++ lab9/lab.js
@@ -7,22 +7,9 @@ Created: 12 May 2024
 License: public domain
 */
 
-$(document).ready(function() {
-    // Add buttons to challange, problems, and result sections
-    $("#challenge").append("<button id='button-challenge'>Make Special</button>");
-    $("#problems").append("<button id='button-problems'>Make Special</button>");
-    $("#results").append("button id='button-results'>Make Speicail</button>");
-
-    // add click listeners to the buttons
-    $("#button-challenge").click(function() {
-        $("#challenge").toggleClass("special");
-    });
-
-    $("#button-problems").click(function() {
-        $("#problems").toggleClass("special");
-    });
-
-    $("#button-results").click(function() {
-        $("#results").toggleClass("special");
-    });
-});
+$("#challenge, #problems, #results").on("click", "button", function() {
+    $(this).parent().toggleClass("special");
+  });
+    $("#challenge, #problems, #results").each(function() {
+      $(this).append("<button class='make-special'>Make Special</button>");
+  });
