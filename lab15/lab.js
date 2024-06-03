84d27f102384c43718b72b8f6b215a93f650683b
julesrules17 <166079237+julesrules17@users.noreply.github.com>
2024-06-02 21:15:31 -0700

    Update lab.js

--- lab15/lab.js
+++ lab15/lab.js
@@ -5,3 +5,17 @@ License: Public Domain
 */
 
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+import { axios } from "@pipedream/platform"
+export default defineComponent({
+  props: {
+    swapi: {
+      type: "app",
+      app: "swapi",
+    }
+  },
+  async run({steps, $}) {
+    return await axios($, {
+      url: `https://swapi.dev/api/films/1/`,
+    })
+  },
+})
